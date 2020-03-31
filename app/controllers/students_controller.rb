521138709445835030cfb757116429bf4c6aa5b4
Warren Harper II <warrenharper2@att.net>
2020-03-31 14:36:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,9 +15,9 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-@student.save
+    @student.save
 
-redirect_to students_path
+redirect_to students_path(@student)
   end
 
 end
